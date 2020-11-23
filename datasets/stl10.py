32824a600bf084ca653f2ffa7c7f604cc9d763ab
Hyungu Kahng <hgkahng@korea.ac.kr>
2020-11-23 07:19:48 +0000

    fix dataset (STL10)

--- datasets/stl10.py
+++ datasets/stl10.py
@@ -9,17 +9,17 @@ class STL10(_STL10):
     num_classes = 10
     def __init__(self,
                  root: str = './data/stl10',
-                 split: str = 'train',
+                 split: str = 'train+unlabeled',
                  transform: object = None,
                  **kwargs):
         if split not in ['train', 'test', 'unlabeled', 'train+unlabeled']:
-            raise ValueError
+            raise ValueError(f"Not a valid split: `{split}`.")
         super(STL10, self).__init__(root=root,
                                     split=split,
                                     folds=None,
                                     transform=transform,
                                     target_transform=None,
-                                    download=True)
+                                    download=False)
 
         assert isinstance(self.data, np.ndarray)
         assert isinstance(self.labels, np.ndarray)
@@ -52,12 +52,14 @@ class STL10Pair(_STL10):
                                         transform=transform,
                                         target_transform=None,
                                         download=False)
+        assert isinstance(self.data, np.ndarray)
+        assert isinstance(self.labels, np.ndarray)
+        self.data = np.transpose(self.data, (0, 2, 3, 1))
 
     def __getitem__(self, idx):
         img, target = self.data[idx], int(self.labels[idx])
-        if self.transform is not None:
-            x1 = self.transform(img)
-            x2 = self.transform(img)
+        x1 = self.transform(img)
+        x2 = self.transform(img)
 
         return dict(x1=x1, x2=x2, y=target, idx=idx)
 
@@ -75,16 +77,52 @@ class STL10ForMoCo(_STL10):
                                            transform=None,
                                            target_transform=None,
                                            download=False)
+        assert isinstance(self.data, np.ndarray)
+        assert isinstance(self.labels, np.ndarray)
+        self.data = np.transpose(self.data, (0, 2, 3, 1))
+        
         self.query_transform = query_transform
         self.key_transform = key_transform
 
     def __getitem__(self, idx):
-        img, target = self.data[idx], int(self.labels[idx])
-        if self.transform is not None:
-            x1 = self.query_transform(img)
-            x2 = self.key_transform(img)
+        img, label = self.data[idx], self.labels[idx]
 
-        return dict(x1=x1, x2=x2, y=target, idx=idx)
+        x1 = self.query_transform(img)
+        x2 = self.key_transform(img)
+
+        return dict(x1=x1, x2=x2, y=label, idx=idx)
+
+
+class STL10ForCLAPP(_STL10):
+    def __init__(self,
+                 root: str = './data/stl10',
+                 split: str = 'train+unlabeled',
+                 query_transform: object = None,
+                 key_transform: object = None,
+                 pseudo_transform: object = None):
+        super(STL10ForCLAPP, self).__init__(root=root,
+                                            split=split,
+                                            folds=None,
+                                            transform=None,
+                                            target_transform=None,
+                                            download=False)
+        assert isinstance(self.data, np.ndarray)
+        assert isinstance(self.labels, np.ndarray)
+        self.data = np.transpose(self.data, (0, 2, 3, 1))
+        
+        self.query_transform = query_transform
+        self.key_transform = key_transform
+        self.pseudo_transform = pseudo_transform
+
+    def __getitem__(self, idx):
+        img, label = self.data[idx], self.labels[idx]
+
+        x1 = self.query_transform(img)
+        x2 = self.key_transform(img)
+        x3 = self.pseudo_transform(img)
+
+        return dict(x1=x1, x2=x2, x3=x3, y=label, idx=idx)
+        
 
 
 STL10ForSimCLR = STL10Pair
