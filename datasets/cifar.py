81e2560e437ed67923f5db2ecd92e320d43d77af
Hyungu Kahng <hgkahng@gmail.com>
2025-01-15 02:08:43 +0900

    code cleanup (cifar10, cifar100)

--- datasets/cifar.py
+++ datasets/cifar.py
@@ -1,8 +1,4 @@
-# -*- coding: utf-8 -*-
 
-import typing
-
-import cv2
 import numpy as np
 import torch
 import torch.nn as nn
@@ -20,27 +16,26 @@ class CIFAR10(_CIFAR10):
                  train: bool = True,
                  transform: nn.Module = None,
                  proportion: float = 1.0,
-                 **kwargs):
-        super(CIFAR10, self).__init__(root=root, train=train, transform=transform)
+                 random_state: int = 42,
+                 ):
+        super().__init__(root=root, train=train, transform=transform)
 
-        self.proportion = proportion
-        if self.proportion < 1.0:
-            indices, _ = train_test_split(
-                np.arange(len(self.data)),
-                train_size=self.proportion,
-                stratify=self.targets,
-                shuffle=True,
-                random_state=2020 + kwargs.get('seed', 0)
-            )
+        if proportion < 1.0:
+            indices, _ = \
+                train_test_split(
+                    np.arange(len(self.data)),
+                    train_size=self.proportion,
+                    stratify=self.targets,
+                    shuffle=True,
+                    random_state=random_state,
+                )
             self.data = self.data[indices]
             self.targets = list(np.array(self.targets)[indices])
 
-        assert isinstance(self.targets, list)
-        assert isinstance(self.data, np.ndarray)
         self.data = self.data.transpose((0, 3, 1, 2))  # BHWC -> BCHW
         self.data = torch.from_numpy(self.data)
 
-    def __getitem__(self, idx: int) -> typing.Dict[str,torch.Tensor]:
+    def __getitem__(self, idx: int) -> dict[str, torch.Tensor]:
         img, target = self.data[idx], self.targets[idx]
         if self.transform is not None:
             img = self.transform(img)
@@ -52,29 +47,27 @@ class CIFAR100(_CIFAR100):
     def __init__(self,
                  root: str,
                  train: bool = True,
-                 transform: object = nn.Module,
+                 transform: nn.Module = None,
                  proportion: float = 1.0,
-                 **kwargs):
-        super(CIFAR100, self).__init__(root=root, train=train, transform=transform)
+                 random_state: int = 42,
+                 ):
+        super().__init__(root=root, train=train, transform=transform)
 
-        self.proportion = proportion
-        if self.proportion < 1.0:
+        if proportion < 1.0:
             indices, _ = train_test_split(
                 np.arange(len(self.data)),
                 train_size=self.proportion,
                 stratify=self.targets,
                 shuffle=True,
-                random_state=2021 + kwargs.get('seed', 0)
+                random_state=random_state,
             )
             self.data = self.data[indices]
             self.targets = list(np.array(self.targets)[indices])
 
-        assert isinstance(self.targets, list)
-        assert isinstance(self.data, np.ndarray)
         self.data = self.data.transpose((0, 3, 1, 2))  # BHWC -> BCHW
         self.data = torch.from_numpy(self.data)
 
-    def __getitem__(self, idx: int) -> typing.Dict[str,torch.Tensor]:
+    def __getitem__(self, idx: int) -> dict[str, torch.Tensor]:
         img, target = self.data[idx], self.targets[idx]
         if self.transform is not None:
             img = self.transform(img)
@@ -83,20 +76,20 @@ class CIFAR100(_CIFAR100):
 
 class CIFAR10Pair(CIFAR10):
     def __init__(self, root: str , train: bool = True, transform: nn.Module = None):
-        super(CIFAR10Pair, self).__init__(root=root, train=train, transform=transform)
+        super().__init__(root=root, train=train, transform=transform)
 
-    def __getitem__(self, idx) -> typing.Dict[str,torch.Tensor]:
+    def __getitem__(self, idx) -> dict[str, torch.Tensor]:
         img, target = self.data[idx], self.targets[idx]
-        x1 = self.transform(img)
-        x2 = self.transform(img)
+        x1 = self.transform(img) if self.transform is not None else img
+        x2 = self.transform(img) if self.transform is not None else img
         return dict(x1=x1, x2=x2, y=target, idx=idx)
 
 
 class CIFAR100Pair(CIFAR100):
     def __init__(self, root: str , train: bool = True, transform: nn.Module = None):
-        super(CIFAR100Pair, self).__init__(root=root, train=train, transform=transform)
+        super().__init__(root=root, train=train, transform=transform)
 
-    def __getitem__(self, idx: int) -> typing.Dict[str,torch.Tensor]:
+    def __getitem__(self, idx: int) -> dict[str, torch.Tensor]:
         img, target = self.data[idx], self.targets[idx]
         x1 = self.transform(img) if self.transform is not None else img
         x2 = self.transform(img) if self.transform is not None else img
@@ -108,12 +101,13 @@ class CIFAR10ForMoCo(CIFAR10):
                  root: str,
                  train: bool = True,
                  query_transform: nn.Module = None,
-                 key_transform: nn.Module = None):
-        super(CIFAR10ForMoCo, self).__init__(root=root, train=train)
+                 key_transform: nn.Module = None,
+                 ):
+        super().__init__(root=root, train=train)
         self.query_transform = query_transform
-        self.key_transform   = key_transform
+        self.key_transform = key_transform
 
-    def __getitem__(self, idx) -> typing.Dict[str,torch.Tensor]:
+    def __getitem__(self, idx: int) -> dict[str, torch.Tensor]:
         img, target = self.data[idx], self.targets[idx]
         x1 = self.query_transform(img) if self.query_transform is not None else img
         x2 = self.key_transform(img) if self.key_transform is not None else img
@@ -125,19 +119,20 @@ class CIFAR100ForMoCo(CIFAR100):
                  root: str,
                  train: bool = True,
                  query_transform: nn.Module = None,
-                 key_transform: nn.Module = None):
-        super(CIFAR100ForMoCo, self).__init__(root=root, train=train)
+                 key_transform: nn.Module = None,
+                 ):
+        super().__init__(root=root, train=train)
         self.query_transform = query_transform
         self.key_transform = key_transform
 
-    def __getitem__(self, idx: int) -> typing.Dict[str,torch.Tensor]:
+    def __getitem__(self, idx: int) -> dict[str, torch.Tensor]:
         img, target = self.data[idx], self.targets[idx]
         x1 = self.query_transform(img) if self.query_transform is not None else img
         x2 = self.key_transform(img) if self.key_transform is not None else img
         return dict(x1=x1, x2=x2, y=target, idx=idx)
 
 
-class CIFAR10ForCLAP(CIFAR10):
+class CIFAR10ForCLAP(CIFAR10):  # TODO: deprecate
     def __init__(self,
                  root: str,
                  train: bool = True,
@@ -145,11 +140,11 @@ class CIFAR10ForCLAP(CIFAR10):
                  key_transform: nn.Module = None,
                  teacher_transform: nn.Module = None):
         super(CIFAR10ForCLAP, self).__init__(root=root, train=train)
-        self.query_transform  = query_transform
-        self.key_transform    = key_transform
+        self.query_transform = query_transform
+        self.key_transform = key_transform
         self.teacher_transform = teacher_transform
 
-    def __getitem__(self, idx: int) -> typing.Dict[str,torch.Tensor]:
+    def __getitem__(self, idx: int) -> dict[str, torch.Tensor]:
         img, target = self.data[idx], self.targets[idx]
         x1 = self.query_transform(img) if self.query_transform is not None else img
         x2 = self.key_transform(img) if self.key_transform is not None else img
@@ -157,7 +152,7 @@ class CIFAR10ForCLAP(CIFAR10):
         return dict(x1=x1, x2=x2, x3=x3, y=target, idx=idx)
 
 
-class CIFAR100ForCLAP(CIFAR100):
+class CIFAR100ForCLAP(CIFAR100):  # TODO: deprecate
     def __init__(self,
                  root: str,
                  train: bool = True,
@@ -169,7 +164,7 @@ class CIFAR100ForCLAP(CIFAR100):
         self.key_transform = key_transform
         self.teacher_transform = teacher_transform
 
-    def __getitem__(self, idx: int) -> typing.Dict[str,torch.Tensor]:
+    def __getitem__(self, idx: int) -> dict[str,torch.Tensor]:
         img, target = self.data[idx], self.targets[idx]
         x1 = self.query_transform(img) if self.query_transform is not None else img
         x2 = self.key_transform(img) if self.key_transform is not None else img
